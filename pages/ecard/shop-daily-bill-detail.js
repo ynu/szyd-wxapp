d1179cfdc86d3719e30d8bbd5184ecf46a2a8a8d
na57 <na57@qq.com>
2018-05-17 15:13:45 +0800

    完成

--- pages/ecard/shop-daily-bill-detail.js
+++ pages/ecard/shop-daily-bill-detail.js
@@ -1,4 +1,6 @@
-// pages/ecard/shop-daily-bill-detail.js
+const { ecardApi } = require('../../utils/utils.js');
+const { formatMoney, formatNumber } = require('../../lib/accounting.js');
+
 Page({
 
   /**
@@ -12,7 +14,46 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData(options);
+
+    wx.showLoading({
+      title: '正在加载',
+      mask: true,
+    });
+    const { shopId, date } = options;
+    Promise.all([
+      // 获取商户账单
+      ecardApi.dailyBill(shopId, date),
+
+      // 获取所有子商户账单
+      ecardApi.dailyBillsForSubShops(shopId, date),
+
+      // 获取设备账单
+      ecardApi.deviceBillsByShop(shopId, date),
+    ]).then(([bill, subShopBills, deviceBills]) => {
+      // 格式化账单数据
+      bill.crAmtText = formatMoney(bill.crAmt, '￥');
+      bill.drAmtText = formatMoney(bill.drAmt, '￥');
+      bill.amtText = formatMoney(bill.crAmt - bill.drAmt, '￥');
+      bill.transCntText = formatNumber(bill.transCnt);
+      this.setData({
+        bill,
+        subShopBills: subShopBills.map(bill => {
+          bill.transCntText = formatNumber(bill.transCnt);
+          bill.amtText = formatMoney(bill.crAmt - bill.drAmt, '￥');
+          return bill;
+        }),
+        subShopCount: subShopBills.length,
+        deviceBills: deviceBills.map(bill => {
+          bill.transCntText = formatNumber(bill.transCnt);
+          bill.amtText = formatMoney(bill.crAmt - bill.drAmt, '￥');
+          return bill;
+        }),
+        deviceCount: deviceBills.length,
+      });
+      wx.hideLoading();
+    });
+
   },
 
   /**
