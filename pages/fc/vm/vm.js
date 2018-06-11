0277830f2f5d44e21257a82f3ebeb9d6519e45a1
liyang <liyang@ynu.edu.cn>
2018-06-11 10:18:34 +0800

    Revert "test"
    This reverts commit a57cc849e6dcd62f843e6e2b9b740307cd840750.

--- pages/fc/vm/vm.js
+++ pages/fc/vm/vm.js
@@ -12,7 +12,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-<<<<<<< HEAD
     console.log(options.colony);
     console.log(fcApi.SelectVmForClusters(options.colony))
     // this.setData({ 
@@ -29,20 +28,3 @@ Page({
   //   });
   // },
 });
-=======
-    var colony = JSON.parse(options.colony)
-    console.log(colony)
-    this.setData({ 
-      datas1: colony[0],
-      datas2: colony[1],
-      
-      })
-  },
-  goToVmOne: function (event) {
-    console.log(event)
-    wx.navigateTo({
-      url: `./vmone?vmone=${event.currentTarget.dataset.vmone}`,
-    })
-  },
-})
->>>>>>> parent of 382606a... 修改fcAPI
