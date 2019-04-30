59debee5bc450322e931a0c58eea9b91516d1b4a
GAHANING <2903095411@qq.com>
2019-04-30 11:27:24 +0800

    修复申请权限的bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,37 +5,38 @@ const {
   uirApi,
   weixinApi,
   appId,
-  Roles,
-} = require('../../utils/utils.js');
-
+  Roles
+} = require("../../utils/utils.js");
+let resData;
 let openIdNew;
 Page({
   data: {
+    resData,
     isUirManager: false,
     loadingOpenId: true,
     openIdNew,
     bills: [],
     zq: {
       firmCount: 141,
-      newsCount: 75334,
+      newsCount: 75334
     },
     fc: {
       vmCount: 401,
       clusterCount: 5,
-      hostCount: 14,
+      hostCount: 14
     },
     ecard: {
       shops: [],
-      devices: [],
+      devices: []
     },
     isFcSupervisor: false,
     isFaSupervisor: false,
-    isEcardSupervisor: false,
+    isEcardSupervisor: false
   },
   initPage() {
     wx.showLoading({
-      title: '正在加载',
-      mask: true,
+      title: "正在加载",
+      mask: true
     });
 
     // 为确保所有promise都能resolve，必须添加catch
@@ -47,57 +48,61 @@ Page({
       fcApi.vmCount().catch(() => 0),
       fcApi.clusters().catch(() => 0),
       fcApi.hostCount().catch(() => 0),
-      weixinApi.getOpenId().catch(() => ''),
-    ]).then(([
-      bills,
-      shops,
-      firmCount,
-      newsCount,
-      vmCount,
-      clusters,
-      hostCount,
-      openId,
-    ]) => {
-      wx.hideLoading();
-
-      // 按日期降序排列
-      bills.sort((a, b) => {
-        if (a.accDate > b.accDate) return -1;
-        else if (a.accDate < b.accDate) return 1;
-        return 0;
-      });
-
-      this.setData({
-        bills,
-        zq: {
+      weixinApi.getOpenId().catch(() => "")
+    ])
+      .then(
+        ([
+          bills,
+          shops,
           firmCount,
           newsCount,
-        },
-        fc: {
           vmCount,
-          clusterCount: clusters.length,
+          clusters,
           hostCount,
-        },
-        ecard: {
-          shops,
-        },
-      });
+          openId
+        ]) => {
+          wx.hideLoading();
 
-      // 设置权限
-      // uirApi.getUser(appId, openId).then((uir) => {
-      //   if (uir && uir.roles && uir.roles.length) {
-      //     // this.setData({
-      //     //   isFcSupervisor: uir.roles.includes(Roles.FcSupervisor),
-      //     //   isEcardSupervisor: uir.roles.includes(Roles.EcardSupervisor),
-      //     // });
-      //   }
-      // }).catch((err) => {
-      //   wx.hideLoading();
-      //   console.error(err);
-      // });
-    }).catch((error) => {
-      wx.hideLoading();
-    });
+          // 按日期降序排列
+          bills.sort((a, b) => {
+            if (a.accDate > b.accDate) return -1;
+            else if (a.accDate < b.accDate) return 1;
+            return 0;
+          });
+
+          this.setData({
+            bills,
+            zq: {
+              firmCount,
+              newsCount
+            },
+            fc: {
+              vmCount,
+              clusterCount: clusters.length,
+              hostCount
+            },
+            ecard: {
+              shops
+            }
+          });
+
+          // 设置权限
+          // uirApi.getUser(appId, openId).then((uir) => {
+          //   if (uir && uir.roles && uir.roles.length) {
+          //     // this.setData({
+          //     //   isFcSupervisor: uir.roles.includes(Roles.FcSupervisor),
+          //     //   isEcardSupervisor: uir.roles.includes(Roles.EcardSupervisor),
+          //     // });
+          //   }
+          // }).catch((err) => {
+          //   wx.hideLoading();
+          //   console.error(err);
+          // });
+        }
+      )
+      .catch(error => {
+        wx.hideLoading();
+      });
   },
 
   onLoad() {
@@ -108,7 +113,7 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() { },
+  onReady() {},
 
   onPullDownRefresh() {
     this.initPage();
@@ -117,11 +122,11 @@ Page({
 
   toApply() {
     wx.showLoading({
-      title: '正在加载',
-      mask: true,
+      title: "正在加载",
+      mask: true
     });
     wx.navigateTo({
-      url: '/pages/application/index',
+      url: "/pages/application/index"
     });
   },
 
@@ -129,31 +134,45 @@ Page({
     const that = this;
     //通过云函数获取当前用户的OPENID
     wx.cloud.callFunction({
-      name: 'getOpenId',
-      complete: (res) => {
+      name: "getOpenId",
+      complete: res => {
         this.setData({
           openIdNew: res.result.OPENID
         });
-      },
+      }
     });
     //获取云端数据库判断当前用户拥有哪些模块的权限
     const db = wx.cloud.database();
-    db.collection('user-permissions').doc(that.data.openIdNew).get().then((res) => {
-      for (let i = 0; i < res.data.roles.length; i++) {
-        if (res.data.roles[i] == '虚拟化平台') {
-          that.setData({
-            isFcSupervisor: true
-          });
-        } else if (res.data.roles[i] == '站群系统') {
-          that.setData({
-            isFaSupervisor: true
-          });
-        } else if (res.data.roles[i] == '一卡通') {
-          that.setData({
-            isEcardSupervisor: true
-          });
+    db.collection("user-permissions")
+      .where({
+        _openid: this.data.openIdNew
+      })
+      .get()
+      .then(res => {
+        if (res.data.length != 0) {
+          db.collection("user-permissions")
+            .where({
+              _openid: this.data.openIdNew
+            })
+            .get()
+            .then(res => {
+              for (let i = 0; i < res.data[0].roles.length; i++) {
+                if (res.data[0].roles[i] == "虚拟化平台") {
+                  that.setData({
+                    isFcSupervisor: true
+                  });
+                } else if (res.data[0].roles[i] == "站群系统") {
+                  that.setData({
+                    isFaSupervisor: true
+                  });
+                } else if (res.data[0].roles[i] == "一卡通") {
+                  that.setData({
+                    isEcardSupervisor: true
+                  });
+                }
+              }
+            });
         }
-      }
-    });
-  },
+      });
+  }
 });
