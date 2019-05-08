fb44edd6cec8245103eb5599002197a74842a953
GAHANING <2903095411@qq.com>
2019-05-08 18:32:04 +0800

    新增站群模块云函数

--- function/fcApiClusters/index.js
+++ function/fcApiClusters/index.js
@@ -6,11 +6,13 @@ cloud.init()
 // 云函数入口函数
 exports.main = async(event, context) => {
   const host = 'http://fc-api.ynu.edu.cn/';
+  //token是由环境变量token1,token2,token3以及.组成的，通过云开发控制台，配置此云函数名分别为token1,token2,token3的键值对环境变量
   const token = `${process.env.token1}.${process.env.token2}.${process.env.token3}`;
+  //siteId通过获取此云函数的环境变量获得，通过云开发控制台，配置此云函数名为siteId的键值对环境变量
   let siteId = process.env.siteId;
   const siteIdOption = event.siteIdOption;
   siteId = siteIdOption || siteId;
-  return new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
     let options = {
       uri: `${host}/site/${siteId}/cluster?token=${token}`,
       json: true
--- function/fcApiHosts/index.js
+++ function/fcApiHosts/index.js
@@ -6,7 +6,9 @@ cloud.init()
 // 云函数入口函数
 exports.main = async(event, context) => {
   const host = 'http://fc-api.ynu.edu.cn/';
+  //token是由环境变量token1,token2,token3以及.组成的，通过云开发控制台，配置此云函数名分别为token1,token2,token3的键值对环境变量
   const token = `${process.env.token1}.${process.env.token2}.${process.env.token3}`;
+  //siteId通过获取此云函数的环境变量获得，通过云开发控制台，配置此云函数名为siteId的键值对环境变量
   let siteId = process.env.siteId;
   const siteIdOptions = event.siteIdOptions;
   let limit = event.limit;
@@ -14,7 +16,7 @@ exports.main = async(event, context) => {
   siteId = siteIdOptions || siteId;
   limit = limit || 20;
   offset = offset || 0;
-  return new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
     let options = {
       uri: `${host}/site/${siteId}/hostResource/?limit=${limit}&offset=${offset}&token=${token}`,
       json: true
