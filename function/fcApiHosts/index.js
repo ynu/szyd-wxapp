f19271f9fc1db931f2badfd4b09d032b10a0dbf2
na57 <na57@qq.com>
2019-05-14 09:40:38 +0800

    出错时返回错误信息

--- function/fcApiHosts/index.js
+++ function/fcApiHosts/index.js
@@ -6,7 +6,7 @@ cloud.init()
 // 云函数入口函数
 exports.main = async(event, context) => {
   try {
-    const host = 'http://fc-api.ynu.edu.cn/';
+    const host = 'http://fc-api.ynu.edu.cn';
    //token通过获取此云函数的环境变量获得，通过云开发控制台，配置此云函数名为token的键值对环境变量
     const token = process.env.token;
     //siteId通过获取此云函数的环境变量获得，通过云开发控制台，配置此云函数名为siteId的键值对环境变量
@@ -23,6 +23,9 @@ exports.main = async(event, context) => {
     };
     return await request(options);
   } catch (err) {
-    console.log(err);
+    return {
+      ret: -1,
+      msg: err,
+    }
   }
 }
