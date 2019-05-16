8a5eb127c99968d740b1c65e4a7a43a50ac3012a
na57 <na57@qq.com>
2019-05-16 10:02:01 +0800

    更新 RisApi

--- utils/utils.js
+++ utils/utils.js
@@ -10,17 +10,17 @@ const WeixinApi = require("../lib/weixin-api.js");
 const EcardApi = require("../lib/ecard-api.js");
 const ZqApi = require("../lib/zq-api.js");
 const FcApi = require("../lib/fc-api.js");
+const RisApi = require('../lib/ris-api.js');
 
 const uirApi = new UirApi();
 const weixinApi = new WeixinApi();
 const ecardApi = new EcardApi();
+const zqApi = new ZqApi();
+const fcApi = new FcApi();
+const risApi = new RisApi();
 
 // 商户管理员角色前置，格式为：ecard:shop-manager:商户ID
 const shopManagerRolePrefix = "ecard:shop-manager:";
-
-const zqApi = new ZqApi();
-
-const fcApi = new FcApi();
 
 const Roles = {
   FcSupervisor: "szyd:fc-supervisor", // 角色权限：进入fc/index
@@ -32,6 +32,7 @@ module.exports = {
   ecardApi,
   zqApi,
   fcApi,
+  risApi,
   Roles,
   shopManagerRolePrefix
 };
