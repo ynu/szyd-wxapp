374bd56585e049a4f6a12898c75a2f3ebd84c30c
GAHANING <2903095411@qq.com>
2019-05-21 15:40:08 +0800

    一卡通权限分类

--- pages/ecard/supervisor-index.js
+++ pages/ecard/supervisor-index.js
@@ -1,19 +1,48 @@
-const { appId, weixinApi } = require('../../utils/utils.js');
-
+const {
+  Roles,
+  shopManagerRolePrefix
+} = require('../../utils/utils.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    isEcardSupervisor: false,//当前用户是否拥有一卡通所有模块权限
+    isEcardOprator: false//当前用户是否拥有日账单查询权限
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    const that = this;
+    const db = wx.cloud.database();
+    //获取当前用户的数据库权限
+    db.collection('user-permissions')
+      .where({
+        _openid: options.openId
+      })
+      .get()
+      .then(res => {
+        const roles = {};
+        res.data[0].roles.forEach(role => {
+          switch (role) {
+            case Roles.EcardSupervisor:
+              roles.isEcardSupervisor = true;
+              break;
+            default:
+            //判断当前用户对此页面按钮的权限
+              if (role.indexOf(shopManagerRolePrefix) != -1) {
+                roles.isEcardOprator = true;
+              }
+          }
+        })
+        if(roles.isEcardSupervisor){
+          roles.isEcardOprator = false
+        }
+        that.setData(roles);
+      })
   },
 
   toDailyBill() {
@@ -29,69 +58,54 @@ Page({
       url: '/pages/ecard/operator-bill-list',
     })
   },
-  
-  toApply() {
-    wx.showLoading({
-      title: '正在加载',
-      mask: true,
-    })
-    weixinApi.getOpenId().then(openid => {
-      console.log(openid);
-      wx.hideLoading();
-      wx.navigateToMiniProgram({
-        appId: 'wx63b32180ec6de471',
-        path: `pages/index/apply?appName=数字云大&appId=${appId}&userId=${openid}&remark=申请一卡通权限`,
-        extraData: {},
-      });
-    });
-  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
