6c68e5c545cbf31c8680e99a62e9fd8c3780ebf4
GAHANING <2903095411@qq.com>
2019-11-06 16:34:32 +0800

    query ecard message

--- pages/ecard/query/index.js
+++ pages/ecard/query/index.js
@@ -2,95 +2,53 @@
 const {
   ecardApi
 } = require('../../../utils/utils.js');
+
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-    info: "",
+    inputShowed: true,
+    inputVal: "",
     resultCount: 0,
     result: []
   },
-
-  inputQuery(e) {
-    this.setData({
-      info: e.detail.value, // 搜索内容一旦变化，之前输入的搜索内容就失效。
-    });
-  },
-
-  queryInfo() {
+  searchInput: function() {
     wx.showLoading({
       title: "正在加载",
       mask: true
     });
+    const db = wx.cloud.database(); //初始化数据库
+    //模糊查询，从对象的name模糊查询得出结果
     Promise.all([
-      ecardApi.cardInfoQuery(this.data.info).catch(() => [])
+      ecardApi.cardInfoQuery(this.data.inputVal).catch(() => [])
     ]).then(
       ([result]) => {
-        console.log(result)
         wx.hideLoading();
-        this.setData({
-          resultCount: result.length,
-          result: result
-        })
+        if (result.length === 0) {
+          wx.showModal({
+            title: '提示',
+            content: '查询结果为空',
+            showCancel: false
+          });
+        } else {
+          this.setData({
+            resultCount: result.length,
+            result: result
+          })
+        }
       }).catch(error => {
       wx.hideLoading();
     });
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-
+  clearInput: function() {
+    this.setData({
+      inputVal: "",
+      result: []
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  inputTyping: function(e) {
+    this.setData({
+      inputVal: e.detail.value,
+      result: [],
+      resultCount: 0
+    });
   }
-})
+});
