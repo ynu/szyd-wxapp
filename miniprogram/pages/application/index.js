7f8f23ea45408bf03b7f098d2585ab30355215ac
GAHANING <2903095411@qq.com>
2020-07-21 10:59:54 +0800

    功能申请界面重写

--- miniprogram/pages/application/index.js
+++ miniprogram/pages/application/index.js
@@ -22,55 +22,168 @@ let modules = [{
   checked: false
 }, {
   name: '数据中心门禁',
-    value: 'szyd:door-supervisor',
+  value: 'szyd:door-supervisor',
   checked: false
-  }, {
-    name: '研究生信息查询',
-    value: 'szyd:yjs-supervisor',
-    checked: false
-  },{
-    name: '本科生信息查询',
-    value: 'szyd:bks-supervisor',
-    checked: false
-  },{
-    name: '统一身份认证系统',
-    value: 'szyd:IdSystem-supervisor',
-    checked: false
-  },{
-    name: '人事',
-    value: 'szyd:rs-supervisor',
-    checked: false
-  }
+}, {
+  name: '研究生信息查询',
+  value: 'szyd:yjs-supervisor',
+  checked: false
+}, {
+  name: '本科生信息查询',
+  value: 'szyd:bks-supervisor',
+  checked: false
+}, {
+  name: '统一身份认证系统',
+  value: 'szyd:IdSystem-supervisor',
+  checked: false
+}, {
+  name: '人事',
+  value: 'szyd:rs-supervisor',
+  checked: false
+}
 ];
-let permissions;
-let length;
-let datas;
-let _id;
-let mobile;
-let sendCode;
+let checkBoxValue; //全局变量复选框值
 Page({
   /**
    * 页面的初始数据
    */
   data: {
+    mobileReceiveVcode: '', //用来存储发送验证码时填写的手机号
     isFirstApply: true, //记录是否为第一申请
-    mobile,
     vcode: {
       btnClass: 'weui-vcode-btn',
       btnText: '获取',
       timer: 0,
     },
-    modules: [],
+    modules: [], //记录模块数组
     permissions: [], //记录当前选择的模块对应的权限
-    //module: '请选择模块', //记录当前选择的模块，默认值为'请选择模块'
-    length, //记录当前用户的云数据库记录数
-    _id,
-    datas: [], //记录已经申请的模块
-    sendCode: '',
+    length: 0, //记录当前用户的云数据库记录数
+    _id: '',
+    sendCode: '',//记录生成的验证码
     dataGet: {
       name: '',
       unit: ''
-    }
+    },
+    formData: {}, //存储表单数据
+    //rules检验规则
+    rules: [{
+      name: 'name',
+      rules: { required: true, message: '姓名必填' },
+    }, {
+      name: 'unit',
+      rules: { required: true, message: '所在单位必填' },
+    }, {
+      name: 'mobile',
+      rules: [{ required: true, message: '手机号必填' }, { mobile: true, message: '手机号格式不对' }],
+    }, {
+      name: 'vcode',
+      rules: { required: true, message: '验证码必填' },
+    }, {
+      name: 'checkbox',
+      rules: { required: true, message: '多选列表是必选项' },
+    }]
+  },
+  //将提示对话框提出，作为一个函数
+  showModal(error) {
+    wx.showModal({
+      content: error.message,
+      showCancel: false,
+    })
+  },
+  //当输入框发生改变时触发此函数
+  formInputChange(e) {
+    const { field } = e.currentTarget.dataset;
+    this.setData({
+      [`formData.${field}`]: e.detail.value
+    });
+  },
+  //点击提交按钮触发此函数
+  submitForm() {
+    const that = this;
+    const db = wx.cloud.database();
+    that.selectComponent('#form').validate((valid, errors) => {
+      //当校验规则不通过时执行if条件为真，执行下面语句
+      if (!valid) {
+        const firstError = Object.keys(errors)
+        if (firstError.length) {
+          that.showModal(errors[firstError[0]]);
+          return false;
+        }
+      } else {
+        //此处的if条件判断验证码与生成的验证码是否一致。
+        if (that.data.formData.vcode !== that.data.sendCode) {
+          wx.showModal({
+            title: '提示',
+            content: '请输入正确的验证码',
+            showCancel: false
+          })
+          return false;
+          //此处的if条件判断收到验证码的手机号与填写的手机号是否一致
+        } else if (that.data.formData.mobile !== that.data.mobileReceiveVcode) {
+          wx.showModal({
+            title: '提示',
+            content: '输入的手机号并非是收到验证码的手机号',
+            showCancel: false
+          })
+          return false;
+        }
+        //如果验证码输入正确执行下面语句
+        else {
+          //判断是否为第一次提交，如果不是执行if条件为真
+          if (that.data.length === 1) {
+            const _ = db.command;
+            db.collection('user-permissions')
+              .doc(that.data._id)
+              .update({
+                data: {
+                  name: that.data.formData.name,
+                  unit: that.data.formData.unit,
+                  mobile: that.data.formData.mobile,
+                  module: that.data.permissions.concat(checkBoxValue),
+                }
+              })
+              .then(() => {
+                wx.showModal({
+                  title: '提示',
+                  content: '您的申请已成功提交，请耐心等候审批',
+                  showCancel: false,
+                  confirmColor: '#007500',
+                  success(res) {
+                    wx.navigateBack({
+                      delta: 1
+                    })
+                  }
+                });
+              });
+            //用户第一次申请
+          } else {
+            db.collection('user-permissions')
+              .add({
+                data: {
+                  name: that.data.formData.name,
+                  unit: that.data.formData.unit,
+                  mobile: that.data.formData.mobile,
+                  module: that.data.permissions.concat(checkBoxValue),
+                  roles: []
+                }
+              })
+              .then(() => {
+                wx.showModal({
+                  title: '提示',
+                  content: '您的申请已成功提交，请耐心等候审批',
+                  showCancel: false,
+                  confirmColor: '#007500',
+                  success(res) {
+                    wx.navigateBack({
+                      delta: 1
+                    })
+                  }
+                });
+              });
+          }
+        }
+      }
+    });
   },
   mobileInput(e) {
     this.setData({
@@ -78,13 +191,23 @@ Page({
       sendCode: '', // 手机号码一旦变化，之前发送的验证码就失效。
     });
   },
+  //用来判断手机号码是否正确
+  isPoneAvailable(arg) {
+    let myreg = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
+    arg = parseInt(arg);
+    if (!myreg.test(arg)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
   btnSendVcode(e) {
     // 如果已经有计时器在运行，则退出
     if (this.data.vcode.timer) return;
     // 验证电话号码是否正确
     const {
       mobile
-    } = this.data;
+    } = this.data.formData;
     if (this.isPoneAvailable(mobile)) {
       wx.showModal({
         title: '手机号码错误',
@@ -100,15 +223,16 @@ Page({
     // 保存验证码
     this.setData({
       sendCode: code,
+      mobileReceiveVcode: mobile
     });
     // 发送验证码
     wx.cloud.callFunction({
-        name: 'qcSmsSendSingle',
-        data: {
-          mobile: this.data.mobile,
-          code: this.data.sendCode
-        }
-      })
+      name: 'qcSmsSendSingle',
+      data: {
+        mobile: this.data.mobile,
+        code: this.data.sendCode
+      }
+    })
       .then(result => {
         // 发送成功
         wx.showToast({
@@ -158,142 +282,30 @@ Page({
   },
   //当申请模块发生改变时触发此函数
   checkboxChange(e) {
-    let val = e.detail.value;
-    val = val.filter(s => s != '');
-    this.setData({
-      permissions: val,
-    });
-  },
-  //用来判断手机号码是否正确
-  isPoneAvailable(arg) {
-    let myreg = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
-    arg = parseInt(arg);
-    if (!myreg.test(arg)) {
-      return true;
+    checkBoxValue = e.detail.value.filter(s => s != '');
+    if (e.detail.value.length === 0) {
+      this.setData({
+        [`formData.checkbox`]: null
+      });
     } else {
-      return false;
-    }
-  },
-  //点击提交按钮触发此函数提交，并检验
-  submitAdd(e) {
-    const db = wx.cloud.database();
-    let count = 0;
-    let conditions = true; //conditions用来记录所需填写的内容是否全部满足
-    const that = this;
-    //如果是第一次申请就需要判断姓名，所属单位是否为空
-    //if (that.data.isFirstApply) {
-    //检验姓名是否为空
-    if (e.detail.value.name.replace(/^\s*|\s*$/g, '') === '') {
-      conditions = false;
-      wx.showModal({
-        title: '提示',
-        content: '姓名不能为空',
-        showCancel: false
+      this.setData({
+        [`formData.checkbox`]: e.detail.value
       });
-      //检验所在单位是否为空
-    } else if (e.detail.value.unit.replace(/^\s*|\s*$/g, '') === '') {
-      conditions = false;
-      wx.showModal({
-        title: '提示',
-        content: '所在单位不能为空',
-        showCancel: false
-      });
-    }
-    // }
-    //检验手机号码填写是否正确
-    else if (that.isPoneAvailable(e.detail.value.mobile)) {
-      conditions = false;
-      wx.showModal({
-        title: '提示',
-        content: '请正确填写手机号',
-        showCancel: false
-      });
-      //检验是否填入了正确的验证码
-    } else if (e.detail.value.vcode != that.data.sendCode || that.data.sendCode === '') {
-      conditions = false;
-      wx.showModal({
-        title: '提示',
-        content: '请输入正确的验证码',
-        showCancel: false
-      })
-      //检验是否选择了模块
-    } else if (that.data.permissions.length === 0) {
-      conditions = false;
-      wx.showModal({
-        title: '提示',
-        content: '请选择模块',
-        showCancel: false
-      });
-    }
-    //当conditions为true时，即所有内容都填完，执行下面代码
-    if (conditions) {
-      //用户不是第一次申请
-      if (that.data.length === 1) {
-        const _ = db.command;
-        db.collection('user-permissions')
-          .doc(that.data._id)
-          .update({
-            data: {
-              name: e.detail.value.name,
-              unit: e.detail.value.unit,
-              mobile: e.detail.value.mobile,
-              module: that.data.permissions
-            }
-          })
-          .then(() => {
-            wx.showModal({
-              title: '提示',
-              content: '您的申请已成功提交，请耐心等候审批',
-              showCancel: false,
-              confirmColor: '#007500',
-              success(res) {
-                wx.navigateBack({
-                  delta: 1
-                })
-              }
-            });
-          });
-        //用户第一次申请
-      } else {
-        db.collection('user-permissions')
-          .add({
-            data: {
-              name: e.detail.value.name,
-              unit: e.detail.value.unit,
-              mobile: e.detail.value.mobile,
-              module: that.data.permissions,
-              roles: []
-            }
-          })
-          .then(() => {
-            wx.showModal({
-              title: '提示',
-              content: '您的申请已成功提交，请耐心等候审批',
-              showCancel: false,
-              confirmColor: '#007500',
-              success(res) {
-                wx.navigateBack({
-                  delta: 1
-                })
-              }
-            });
-          });
-      }
     }
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {},
+  onLoad: function (options) { },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {},
+  onReady: function () { },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     const db = wx.cloud.database();
     const that = this;
     wx.showLoading({
@@ -307,25 +319,20 @@ Page({
         modules: modules
       });
       if (res.data.length != 0) {
-        for (let i = 0; i < res.data[0].roles.length; i++) {
-          for (let j = 0; j < modules.length; j++) {
-            if (res.data[0].roles[i] === modules[j].value) {
-              modules[j].disabled = true;
-            }
-          }
-        }
-        for (let i = 0; i < res.data[0].module.length; i++) {
-          for (let j = 0; j < modules.length; j++) {
-            if (res.data[0].module[i] === modules[j].value) {
-              modules[j].checked = true;
-            }
-          }
+        for (const key in res.data[0].module) {
+          modules = modules.reduce((total, current) => {
+            current.value !== res.data[0].module[key] && total.push(current);
+            return total;
+          }, []);
         }
         that.setData({
+          [`formData.name`]: res.data[0].name, //初始化校验数据name
+          [`formData.unit`]: res.data[0].unit, //初始化校验数据unit
+          [`formData.mobile`]: res.data[0].mobile, //初始化校验数据mobile
+          permissions: res.data[0].module || [],
           dataGet: res.data[0],
           mobile: res.data[0].mobile,
           modules: modules,
-          datas: res.data[0].module,
           isFirstApply: false,
           _id: res.data[0]._id
         });
@@ -337,25 +344,25 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {},
+  onHide: function () { },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {},
+  onUnload: function () { },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {},
+  onPullDownRefresh: function () { },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {},
+  onReachBottom: function () { },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {}
+  onShareAppMessage: function () { }
 });
